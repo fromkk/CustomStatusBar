7e824258cc11ce302575e3d68f4ef32eb1e54a5c
Kazuya Ueoka <k-ueoka@atgs.jp>
2013-06-21 09:43:42 +0900

    Initial Commit

---
+++ CustomStatusBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomStatusBar
+//
+//  Created by Kazuya Ueoka on 2013/06/21.
+//  Copyright (c) 2013年 fromkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomStatusBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomStatusBar
+//
+//  Created by Kazuya Ueoka on 2013/06/21.
+//  Copyright (c) 2013年 fromkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
